5388be91eaa8444ddf2191aa93d3e047026ad8ef
ACER <ACER@DESKTOP-RNQ4RHF>
2021-11-19 23:25:13 +0530

    second commit

--- GitSample/src/com/git/sample/Sample.java
+++ GitSample/src/com/git/sample/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is sample");
+		System.out.println("this is sample1");
 
 	}
 
