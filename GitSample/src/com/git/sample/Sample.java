49e73d50f07bc641fb860a1d451f1deded48e5f4
ACER <ACER@DESKTOP-RNQ4RHF>
2021-11-20 08:57:00 +0530

    third change

--- GitSample/src/com/git/sample/Sample.java
+++ GitSample/src/com/git/sample/Sample.java
@@ -6,6 +6,7 @@ public class Sample {
 		// TODO Auto-generated method stub
 		System.out.println("this is sample");
 		System.out.println("this is sample1");
+		System.out.println("this is sample2");
 
 	}
 
